a767b5f27f497be3cc0df1f28b8cb7c3684a1ba9
GioDev <ga5715303@gmail.com>
2025-11-23 17:07:18 -0600

    reemplazar nodemailer por mailersend

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -1,43 +1,63 @@
-import { transporter } from "../config/nodemailer"
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const mailerSend = new MailerSend({
+  apiKey: process.env.MAILERSEND_API_KEY!,
+});
 
 interface IEmail {
-    email: string
-    name: string
-    token: string
+  email: string;
+  name: string;
+  token: string;
 }
 
 export class AuthEmail {
-    static sendConfirmationEmail = async ( user : IEmail ) => {
-        const info = await transporter.sendMail({
-            from: 'Task Flow <contacto@gersonamaya.com>',
-            to: user.email,
-            subject: 'Task Flow - Confirma tu cuenta',
-            text: 'Task Flow - Confirma tu cuenta',
-            html: `<p>Hola: ${user.name}, has creado tu cuenta en Task Flow, ya casi esta todo listo, solo debes confirmar tu cuenta</p>
-                <p>Visita el siguiente enlace:</p>
-                <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
-                <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
-        })
+  static sendConfirmationEmail = async (user: IEmail) => {
+    try {
+        const sentFrom = new Sender("contacto@gersonamaya.com", "Task Flow");
+        const recipients = [new Recipient(user.email)];
 
-        console.log('Mensaje enviado', info.messageId)
+        const emailParams = new EmailParams()
+        .setFrom(sentFrom)
+        .setTo(recipients)
+        .setSubject("Task Flow - Confirma tu cuenta")
+        .setHtml(`
+            <p>Hola: ${user.name}, has creado tu cuenta en Task Flow, ya casi está todo listo, solo debes confirmar tu cuenta.</p>
+            <p>Visita el siguiente enlace:</p>
+            <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
+            <p>E ingresa el código: <b>${user.token}</b></p>
+            <p>Este token expira en 10 minutos</p>
+        `)
+        .setText("Task Flow - Confirma tu cuenta");
+
+        await mailerSend.email.send(emailParams);
+    } catch (error) {
+        console.error("ERROR al enviar email:", error?.body || error);
     }
+  };
 
-    static sendPasswordResetToken = async ( user : IEmail ) => {
-        const info = await transporter.sendMail({
-            from: 'Task Flow <contacto@gersonamaya.com>',
-            to: user.email,
-            subject: 'Task Flow - Reestablece tu password',
-            text: 'Task Flow - Reestablece tu password',
-            html: `<p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
-                <p>Visita el siguiente enlace:</p>
-                <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer Password</a>
-                <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
-        })
+  static sendPasswordResetToken = async (user: IEmail) => {
+    try {
+        const sentFrom = new Sender("contacto@gersonamaya.com", "Task Flow");
+        const recipients = [new Recipient(user.email)];
 
-        console.log('Mensaje enviado', info.messageId)
+        const emailParams = new EmailParams()
+        .setFrom(sentFrom)
+        .setTo(recipients)
+        .setSubject("Task Flow - Reestablece tu password")
+        .setHtml(`
+            <p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
+            <p>Visita el siguiente enlace:</p>
+            <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer Password</a>
+            <p>E ingresa el código: <b>${user.token}</b></p>
+            <p>Este token expira en 10 minutos</p>
+        `)
+        .setText("Task Flow - Reestablece tu password");
+
+        await mailerSend.email.send(emailParams);
+    } catch (error) {
+        console.error("ERROR al enviar email:", error?.body || error);
     }
+  };
 }
